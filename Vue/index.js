23a49a60e554b40a9da4008298a4a6e8645e8306
mfmNick15 <mario_fonseca15@hotmail.com>
2018-07-12 14:07:37 -0600

    Angus Page

---
+++ Vue/index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+       todos:[
+           {text: 'Hola soy el uno'},
+           {text: 'Hola soy el Segundo'},
+           {text: 'Hola creo que soy el ultimo'}
+
+       ]
+    }
+});
